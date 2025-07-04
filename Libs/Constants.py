b07276f1c2b1d1fcb4770f25cf320640f463105f
giorgio <giorgio.domen@gmail.com>
2025-07-04 09:57:44 +0200

    read data from Zenodo and build models

---
+++ Libs/Constants.py
@@ -0,0 +1,8 @@
+ang2bohr=1.8897261246
+bohr2ang=.5291772109
+
+dalton_to_au=  1.660e-27 / 9.109e-31
+plankAU=6.283185307179586
+lightspeedAU=137.036
+centimeter2bohr=1.8897261246e+8
+Ha2KCal=627.509474
